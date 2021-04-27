85807eefcbe8028c09301495bed8c02cc54f3e78
KateIsabelle <kate.davis887@gmail.com>
2021-04-27 16:08:23 +0000

    calculate store revenues exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,11 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+puts "TOTAL REVENUE: #{total_revenue}"
+
+average_revenue = Store.average(:annual_revenue)
+puts "AVERAGE REVENUE: #{average_revenue}"
+
+successful_stores_count = Store.where("annual_revenue > #{1000000}").count
+puts "How many stores sell over 1000000$? --> #{successful_stores_count}"
