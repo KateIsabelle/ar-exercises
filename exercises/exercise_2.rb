1aad4951ab200904b3db726ccd982073bc6b12a2
KateIsabelle <kate.davis887@gmail.com>
2021-04-27 14:42:14 +0000

    update first store's name exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,11 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+puts "Store 2 name: #{@store2.name}"
+
+puts "Store 1 old name: #{@store1.name}" # Burnaby
+@store1.update(name: "Kensington")
+puts "Store 1 new name: #{@store1.name}" # Kensington
+
