b69d1cb1d1c119a4f33464a8c80a48db8d4ef8b9
KateIsabelle <kate.davis887@gmail.com>
2021-04-27 15:40:39 +0000

    fetch a subset of stores exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,20 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+surry = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+@womens_stores_low_revenue = Store.where("annual_revenue < #{1000000}", womens_apparel: true)
+
+
+puts "Stores carrying men's apparel -->"
+@mens_stores.each do |store|
+  puts "#{store.name}: #{store.annual_revenue}"
+end
+
+puts "Stores with low revenue carrying women's apparel -->"
+@womens_stores_low_revenue.each do |store|
+  puts "#{store.name}: #{store.annual_revenue}"
+end
