d8c9bf2924928a3feb5195deb007a967cb0214df
KateIsabelle <kate.davis887@gmail.com>
2021-04-27 14:44:42 +0000

    delete store 3 exercise 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -5,4 +5,8 @@ require_relative './exercise_2'
 puts "Exercise 3"
 puts "----------"
 
-# Your code goes here ...
+@store3 = Store.find_by(id: 3)
+
+puts "Count before delete: #{Store.all.count}" # 3
+@store3.destroy
+puts "Count after delete: #{Store.all.count}" # 2
